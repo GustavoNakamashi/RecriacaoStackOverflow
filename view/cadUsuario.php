6cc9d84257a8b999395221df37b7f8bba0705b7e
DaviAmaral <amaraldavi516@gmail.com>
2026-05-16 01:06:37 -0300

    organizando

--- view/cadUsuario.php
+++ view/cadUsuario.php
@@ -4,10 +4,13 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>StackOverflow</title>
+    <title>CadUsuario</title>
+
+    <link rel="stylesheet" href="../css/styleCadUsuario.css">
+    
 
 </head>
 <body>
-    
+    <p> hello word <p>
 </body>
 </html>
---
+++ view/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teste</title>
+
+    <link rel="stylesheet" href="../css/styleHome.css">
+
+</head>
+
+<body>
+    <ul>
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#Quest">Questões</a></li>
+        <li><a href="#Tags">Etiquetas</a></li>
+        <li><a href="#desaf">Desafios</a></li>
+        <li><a href="#Artigos">Artigos</a></li>
+    </ul>
+
+    <div>
+        <h1> Inteligência Artificial </h1>
+        
+        <p></p>
+
+    </div>
+
+</body>
+</html>
