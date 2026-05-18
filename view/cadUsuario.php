e4628e33d84a4c336326ea504838c77c0f7d5435
DaviAmaral <amaraldavi516@gmail.com>
2026-05-17 21:56:12 -0300

    Iniciando

--- view/cadUsuario.php
+++ view/cadUsuario.php
@@ -11,6 +11,33 @@
 
 </head>
 <body>
-    <p> hello word <p>
+
+    <section class="tela-cadastro ">
+        <div>
+            <h2> Participe do StackOverflow <h2>
+            
+            <p>Ao clicar em "Cadastrar-se",</p>
+
+            <form>
+
+                <label>Nome</label><br>
+                <input type="text" placeholder="Nome" name="inputNome"><br>
+            
+                <label>Email</label><br>
+                <input type="text" placeholder="Email" name="inputEmail"><br>
+
+                <label>Senha</label><br>
+                <input type="password" placeholder="Senha" name="inputSenha"><br>
+
+                <button>Cadastrar-se</button><br>
+
+                <p>Já tem uma conta? Iniciar sessão</p>
+
+            </form>
+        </div>    
+
+    </section>
+
+
 </body>
 </html>
