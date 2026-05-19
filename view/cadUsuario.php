787c5fbfff9f6e55a479ef3bbe42fc1cd05e9140
DaviAmaral <amaraldavi516@gmail.com>
2026-05-19 08:06:09 -0300

    ajustando logo

--- view/cadUsuario.php
+++ view/cadUsuario.php
@@ -14,30 +14,37 @@
 
     <section id="centralizando-tela-cadastro">
         <div class="tela-cadastro">
-            
-            <img src=../img/logo.png>
-            <h2> Participe do StackOverflow <h2>
-            
-            <p style="font-size: 12px; font-family: Inter, sans-serif; font-weight: normal; color: #333; max-width: 440px;">
-            Ao clicar em "Cadastrar-se", você concorda com nossos termos de serviço e confirma que leu nossa política de privacidade .
-            </p>
 
-            <form>
+            <div class="header-cadastro">
+                <img src=../img/logo.png>
+                <h2> <span style="font-family: inter"> 
+                    Participe do StackOverflow </span> </h2>
 
-                <label>Nome</label><br>
-                <input type="text" placeholder="Nome" name="inputNome"><br>
-            
-                <label>Email</label><br>
-                <input type="text" placeholder="Email" name="inputEmail"><br>
+            </div>
 
-                <label>Senha</label><br>
-                <input type="password" placeholder="Senha" name="inputSenha"><br>
+            <div class="conteudo-cadastro">
 
-                <button>Cadastrar-se</button><br>
+                <p style="font-size: 12px; font-family: Inter, sans-serif; font-weight: normal; color: #333; max-width: 440px;">
+                Ao clicar em "Cadastrar-se", você concorda com nossos termos de serviço e confirma que leu nossa política de privacidade .
+                </p>
 
-                <p id="frase-final">Já tem uma conta? <a href="#" style=" text-decoration: none;"> <span style="color: blue;"> Iniciar sessão </span> </a></p>
+                <form>
 
-            </form>
+                    <label>Nome</label><br>
+                    <input type="text" placeholder="Nome" name="inputNome"><br>
+                
+                    <label>Email</label><br>
+                    <input type="text" placeholder="Email" name="inputEmail"><br>
+
+                    <label>Senha</label><br>
+                    <input type="password" placeholder="Senha" name="inputSenha"><br>
+
+                    <button>Cadastrar-se</button><br>
+
+                    <p id="frase-final">Já tem uma conta? <a href="#" style=" text-decoration: none;"> <span style="color: blue;"> Iniciar sessão </span> </a></p>
+
+                </form>
+            <div>
         </div>    
 
     </section>
