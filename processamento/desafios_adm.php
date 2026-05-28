63d493f1fa96999f78e4679acc06b23d08990a75
DaviAmaral <amaraldavi516@gmail.com>
2026-05-28 08:19:02 -0300

    teste

--- processamento/desafios_adm.php
+++ processamento/desafios_adm.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/../config/conexao2.php';
-if ($_SESSION['usuario_id'] != 1) { header('Location: ../view/home.php'); exit; }
+if ($_SESSION['usuario_id'] != 0) { header('Location: ../view/home.php'); exit; }
 
 if ($_POST['acao'] == 'criar') {
     $stmt = $pdo->prepare("INSERT INTO desafios (titulo, descricao, dificuldade) VALUES (?, ?, ?)");
